a9f77000c72d27b0ea68a847766012e0cdde2790
harshitsinghai <harshitsinghai77@gmail.com>
2025-10-07 11:34:55 +0530

    Add github action

--- cdk_app.py
+++ cdk_app.py
@@ -1,17 +1,13 @@
 import os
-
-from dotenv import load_dotenv
 import aws_cdk as cdk
 from infrastructure.dynamodb_stack import NemoAIDynamoDBStack
-
-load_dotenv()
 
 app = cdk.App()
 
 NemoAIDynamoDBStack(
     app,
     "NemoAIDynamoDBStack",
-    env=cdk.Environment(account=os.getenv("CDK_DEFAULT_ACCOUNT"), region="us-east-1"),
+    env=cdk.Environment(account=os.getenv("AWS_DEFAULT_ACCOUNT"), region="us-east-1"),
 )
 
 app.synth()
